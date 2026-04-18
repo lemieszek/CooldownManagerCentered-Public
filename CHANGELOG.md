20cc0b0d40c35329aaf5991287119e04d0d8df6d
lemieszek <dawid.lemieszek.1@gmail.com>
2026-04-18 15:41:59 +0200

    v2.6.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Cooldown Manager Centered
 
-## [v2.6.4](https://github.com/lemieszek/CooldownManagerCentered/tree/v2.6.4) (2026-04-12)
-[Full Changelog](https://github.com/lemieszek/CooldownManagerCentered/compare/v2.6.3...v2.6.4) 
+## [v2.6.6](https://github.com/lemieszek/CooldownManagerCentered/tree/v2.6.6) (2026-04-18)
+[Full Changelog](https://github.com/lemieszek/CooldownManagerCentered/compare/v2.6.5...v2.6.6) 
 
-- v2.6.4  
+- v2.6.6 better is onActualCooldown check  
--- modules/cooldownStyle.lua
+++ modules/cooldownStyle.lua
@@ -26,9 +26,9 @@ local desaturationCurve = C_CurveUtil.CreateCurve()
 desaturationCurve:AddPoint(0, 0)
 desaturationCurve:AddPoint(0.001, 1)
 
-local notOnCDCurve = C_CurveUtil.CreateCurve()
-notOnCDCurve:AddPoint(0, 1)
-notOnCDCurve:AddPoint(0.001, 0)
+local isZeroCurve = C_CurveUtil.CreateCurve()
+isZeroCurve:AddPoint(0, 1)
+isZeroCurve:AddPoint(0.001, 0)
 
 local function GetConfiguredGlowStyle()
     local style = ns.db.profile.cooldownManager_experimental_glow_style or GLOW_STYLE_DEFAULT
@@ -285,7 +285,7 @@ local function UpdateButtonGlowState(cdmFrame, value)
             end
             local spellID = cooldownInfo.overrideSpellID or cooldownInfo.spellID
             local cooldownDuration = C_Spell.GetSpellChargeDuration(spellID)
-            local alpha = cooldownDuration:EvaluateRemainingDuration(notOnCDCurve)
+            local alpha = cooldownDuration:EvaluateRemainingDuration(isZeroCurve)
             glow:SetAlpha(alpha)
             return
         end
@@ -318,7 +318,7 @@ local function UpdateButtonGlowState(cdmFrame, value)
             return
         end
         local cooldownDuration = C_Spell.GetSpellCooldownDuration(spellID)
-        local alpha = cooldownDuration:EvaluateRemainingDuration(notOnCDCurve)
+        local alpha = cooldownDuration:EvaluateRemainingDuration(isZeroCurve)
         if glow then
             glow:SetAlpha(alpha)
         end
@@ -351,10 +351,20 @@ local function ApplyIconSettings(cdmFrame)
         shouldShowAuras = false
     end
 
+    local spellCharges = C_Spell.GetSpellCharges(spellID)
+    local hasCharges = spellCharges and spellCharges.maxCharges > 1
+    local cooldown = C_Spell.GetSpellCooldown(spellID)
     if shouldShowAuras and cdmFrame.wasSetFromAura then
         cdmFrame.Cooldown:SetDrawSwipe(cdmFrame.cooldownShowSwipe == true)
         if ns.db.profile.cooldownManager_desaturate_under_aura then
-            cdmFrame.Icon:SetDesaturation(1)
+            if hasCharges then
+                local isOnActualCooldown = cooldown.isActive and not cooldown.isOnGCD
+                if isOnActualCooldown then
+                    cdmFrame.Icon:SetDesaturation(1)
+                end
+            else
+                cdmFrame.Icon:SetDesaturation(1)
+            end
         else
             cdmFrame.Icon:SetDesaturation(0)
         end
@@ -371,18 +381,7 @@ local function ApplyIconSettings(cdmFrame)
     local guesstimateThatSpellWithChargesIsOnChargeCooldown = cdmFrame.CooldownFlash
         and cdmFrame.CooldownFlash:IsShown()
     if cdmFrame.wasSetFromAura then
-        local spellCharges = C_Spell.GetSpellCharges(spellID)
-        local hasCharges = spellCharges and spellCharges.maxCharges > 1
-
         if hasCharges then
-            --[[ -- potentially simplified code, to just leave it as:
-            cdmFrame.Cooldown:SetDrawSwipe(guesstimateThatSpellWithChargesIsOnChargeCooldown)
-            cdmFrame.Cooldown:SetDrawEdge(
-                not guesstimateThatSpellWithChargesIsOnChargeCooldown
-                    or CooldownStyle.GetAlwaysShowCooldownEdge(baseSpellId)
-            )
-            ]]
-
             --[[
             TODO  check new blizzard changes:
             Action/Spell cooldown APIs now return isEnabled and maxCharges as non-secrets.
@@ -435,6 +434,7 @@ local function ApplyCooldownSettings(cdmFrame)
         cdmFrame.Cooldown:SetDrawEdge(true)
     end
 
+    local cooldown = C_Spell.GetSpellCooldown(spellID)
     if shouldShowAuras and cdmFrame.wasSetFromAura then
         cdmFrame.Cooldown:SetReverse(CooldownStyle.GetReverseAuraSwipe(baseSpellId))
         if ns.db.profile.cooldownManager_customSwipeColor_enabled then
@@ -442,12 +442,21 @@ local function ApplyCooldownSettings(cdmFrame)
             cdmFrame.Cooldown:SetSwipeColor(_r, _g, _b, _a)
         end
         if ns.db.profile.cooldownManager_desaturate_under_aura then
-            cdmFrame.Icon:SetDesaturation(1)
+            local spellCharges = C_Spell.GetSpellCharges(spellID)
+
+            local hasCharges = spellCharges and spellCharges.maxCharges > 1
+            if hasCharges then
+                local isOnActualCooldown = cooldown.isActive and not cooldown.isOnGCD
+                if isOnActualCooldown then
+                    cdmFrame.Icon:SetDesaturation(1)
+                end
+            else
+                cdmFrame.Icon:SetDesaturation(1)
+            end
         end
         return
     end
     local shouldHideAuras = not shouldShowAuras and cdmFrame.wasSetFromAura
-
     cdmFrame.Cooldown:SetReverse(false)
 
     if ns.db.profile.cooldownManager_customSwipeColor_enabled then
@@ -457,8 +466,6 @@ local function ApplyCooldownSettings(cdmFrame)
         -- from CooldownViewerConstants.ITEM_COOLDOWN_COLOR
         cdmFrame.Cooldown:SetSwipeColor(0, 0, 0, 0.7)
     end
-
-    local cooldown = C_Spell.GetSpellCooldown(spellID)
 
     cdmFrame._CMCTracker_Desaturation = nil
 
@@ -537,6 +544,7 @@ local function HookCooldownFrame(cdmFrame)
 
     hooksecurefunc(cdmFrame.Icon, "SetDesaturated", function(self, secretValue)
         local cdmFrame = self:GetParent()
+
         if cdmFrame.wasSetFromAura then
             UpdateButtonGlowState(cdmFrame)
         else
@@ -547,7 +555,6 @@ local function HookCooldownFrame(cdmFrame)
     end)
 
     -- if FIX_BLIZZARD_MISSING_DEBUFF[cooldownInfo.spellID] then
-    --     print("Applying fix for missing desaturation for spellID", cooldownInfo.spellID)
     --     hooksecurefunc(cdmFrame.Cooldown, "Clear", function(self)
     --         ApplyCooldownSettings(self:GetParent(), true)
     --     end)
